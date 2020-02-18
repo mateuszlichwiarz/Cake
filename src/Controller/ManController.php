bb47384ca98bbaddc7f295e515f41318617e07eb
lichwiarz <mateusz.lichwiarz@gmail.com>
2020-02-18 13:01:39 +0100

    test code

--- src/Controller/ManController.php
+++ src/Controller/ManController.php
@@ -6,6 +6,12 @@ class ManController extends AppController
 {
 	public function index()
 	{
-		
+		$man = array();
+		$man['name'] = 'Mateusz';
+		$man['weight'] = '80';
+		$man['energy'] = '80';
+		$man['famine'] = '5';
+
+		$this->set(compact('man'));
 	}
 }
