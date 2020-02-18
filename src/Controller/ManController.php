deaa56e56c939dfc5e81c47e0acdfc3ff70dd658
lichwiarz <mateusz.lichwiarz@gmail.com>
2020-02-18 13:19:51 +0100

    new man action in man controller

--- src/Controller/ManController.php
+++ src/Controller/ManController.php
@@ -14,4 +14,25 @@ class ManController extends AppController
 
 		$this->set(compact('man'));
 	}
+
+	public function newMan()
+	{
+		$this->loadComponent('Flash');
+		
+		$man = $this->Mans->newEmptyEntity();
+		if($this->request->is('post')) {
+			$man = $this->Mans->patchEntity($man, $this->request->getData());
+			$man->name = 'John';
+			$man->weight = '80';
+			$man->energy = '100';
+			$man->famine = '0';
+
+		if($this->Mans->save($man)){
+			$this->Flash->success(__('Man saved'));
+			return $this->redirect(['action' => 'index']);
+		}
+		$this->Flash->error(__('Unable to create your human.'));
+		}
+		$this->set('man', $man);
+	}
 }
