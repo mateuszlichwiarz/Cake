3808174454b6568289bc3d76508b52930a07acf2
lichwiarz <mateusz.lichwiarz@gmail.com>
2020-02-19 11:11:11 +0100

    display data in index template

--- templates/Man/index.php
+++ templates/Man/index.php
@@ -1,7 +1,11 @@
-<h2>Man:</h2>
-<h3>
-<pre>
-<?= print_r($man) ?>
-<pre>
-</h3>
+<div class='man'>
+	<h2>Man</h2>
+<?php foreach($man as $item): ?>
+	<h4> <?= h($item->name) ?> </h4>
+	<p>Weight: <?= h($item->weight) ?> </p>
+	<p>Famine: <?= h($item->famine) ?> </p>
+	<p>Energy: <?= h($item->energy) ?> </p>
+	<br>
+<?php endforeach; ?>
+</div>
 <p><?= $this->Html->link('Create', ['action' => 'newMan']) ?></p>
