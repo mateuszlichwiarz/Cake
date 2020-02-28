784ba656d08734c640a91545221d9f6a806f5cf5
lichwiarz <mateusz.lichwiarz@gmail.com>
2020-02-28 23:39:47 +0100

    Temporary body of starver class

---
+++ src/Human/StarverClass.php
@@ -0,0 +1,31 @@
+<?php
+
+// IMPORTANT
+// only my ideas, not working
+// ALL TO REFACTORING
+namespace App\Human;
+
+use App\TimeClass;
+
+class StarverClass
+{
+	private $id;
+	private $name;
+	private $energy;
+	private $famine;
+
+	public function main()
+	{
+		$currentHour = date('H');
+		$futureHour = $currentHour('H') + 1;
+
+		if($currentHour == $futureHour){
+			return $famine = $this->starvation();
+		}
+	}
+
+	public function starvation()
+	{
+		return $famine += 25;
+	}
+}
